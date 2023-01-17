3ef49a9319da6249e421e948c15918b39dd4948e
Sakib Rahman <rahmans@myumanitoba.ca>
2023-01-17 12:48:25 -0500

    Proper way to access indexing

--- detectorGenerator.py
+++ detectorGenerator.py
@@ -39,12 +39,12 @@ ring = [5,5,5,6,4,3,2,1]
 if args.generate_quartz:
   for i in range(0, len(front_quartz.index)):
     gdml += "<xtru name=\"quartz_"+str(ring[i])+"\"> \n \
-    <twoDimVertex x=\""+front_quartz['USLCWz'][i]+"\" y=\""+front_quartz['USLCWy'][i]+"\"/> \n\
-    <twoDimVertex x=\""+front_quartz['DSLCWz'][i]+"\" y=\""+front_quartz['DSLCWy'][i]+"\"/> \n\
-    <twoDimVertex x=\""+front_quartz['DSUCWz'][i]+"\" y=\""+front_quartz['DSUCWy'][i]+"\"/> \n\
-    <twoDimVertex x=\""+front_quartz['USUCWz'][i]+"\" y=\""+front_quartz['USUCWy'][i]+"\"/> \n\
-    <section zOrder=\"0\" zPosition=\""+front_quartz['USLCWx'][i]+"\" xOffset=\"0\" yOffset=\"0\" scalingFactor=\"1.0\"/> \n\
-    <section zOrder=\"1\" zPosition=\""+front_quartz['USLCCWx'][i]+"\" xOffset=\"0\" yOffset=\"0\" scalingFactor=\"1.0\"/> \n\
+    <twoDimVertex x=\""+front_quartz['USLCWz'].iloc[i]+"\" y=\""+front_quartz['USLCWy'].iloc[i]+"\"/> \n\
+    <twoDimVertex x=\""+front_quartz['DSLCWz'].iloc[i]+"\" y=\""+front_quartz['DSLCWy'].iloc[i]+"\"/> \n\
+    <twoDimVertex x=\""+front_quartz['DSUCWz'].iloc[i]+"\" y=\""+front_quartz['DSUCWy'].iloc[i]+"\"/> \n\
+    <twoDimVertex x=\""+front_quartz['USUCWz'].iloc[i]+"\" y=\""+front_quartz['USUCWy'].iloc[i]+"\"/> \n\
+    <section zOrder=\"0\" zPosition=\""+front_quartz['USLCWx'].iloc[i]+"\" xOffset=\"0\" yOffset=\"0\" scalingFactor=\"1.0\"/> \n\
+    <section zOrder=\"1\" zPosition=\""+front_quartz['USLCCWx'].iloc[i]+"\" xOffset=\"0\" yOffset=\"0\" scalingFactor=\"1.0\"/> \n\
     </xtru> \n\n\
     "
 
