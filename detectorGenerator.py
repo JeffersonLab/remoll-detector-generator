05fa9b7c6c8fb28612d0a8ea86d5ca0c01e6ff6b
Sakib Rahman <rahmans@myumanitoba.ca>
2023-01-17 11:47:35 -0500

    Modify input interface to read csv coordinates and add dummy output file

--- detectorGenerator.py
+++ detectorGenerator.py
@@ -12,103 +12,27 @@ parser = argparse.ArgumentParser(description="Generate a detector array based on
 parser.add_argument("--generate-quartz", dest="generate_quartz", action="store", required=False, default=True, help="Set to true if you want to generate all the quartz pieces")
 parser.add_argument("--generate-lg", dest="generate_lg", action="store", required=False, default=False, help="Set to true if you want to generate all lightguides")
 parser.add_argument("--generate-support", dest="generate_support", action="store", required=False, default=False, help="Set to true if you want to generate all supports")
-
+parser.add_argument("--front-quartz", dest="front_quartz", action="store", required=False, default="FF.csv", help="Set the csv file containing coordinates of quartz piece of front modules")
+parser.add_argument("--back-quartz", dest="back_quartz", action="store", required=False, default="BF.csv", help="Set the csv file containing coordinates of quartz piece of back modules")
+parser.add_argument("--out-file", dest="out_file", action="store", required=False, default="detector.gdml", help="Set the name of output gdml file")
 args=parser.parse_args()
 
+front_quartz = pd.read_csv(args.front_quartz)
+back_quartz  = pd.read_csv(args.back_quartz)
 
-qz_thick = {"R1F": 20,
-            "R1B": 20,
-            "R2F": 20,
-            "R2B": 20,
-            "R3F": 20,
-            "R3B": 20,
-            "R4F": 20,
-            "R4B": 20,
-            "R5F1": 17,
-            "R5F2": 17,
-            "R5F3": 17,
-            "R5B1": 17,
-            "R5B2": 17,
-            "R5B3": 17,
-            "R6F": 20,
-            "R6B": 20}
-qz_llc =  {"R1F": np.array([]),
-           "R1B": np.array([]),
-           "R2F": np.array([]),
-           "R2B": np.array([]),
-           "R3F": np.array([]),
-           "R3B": np.array([]),
-           "R4F": np.array([]),
-           "R4B": np.array([]),
-           "R5F1": np.array([]),
-           "R5F2": np.array([]),
-           "R5F3": np.array([]),
-           "R5B1": np.array([]),
-           "R5B2": np.array([]),
-           "R5B3": np.array([]),
-           "R6F": np.array([]),
-           "R6B": np.array([])}
-qz_lrc =  {"R1F": np.array([]),
-           "R1B": np.array([]),
-           "R2F": np.array([]),
-           "R2B": np.array([]),
-           "R3F": np.array([]),
-           "R3B": np.array([]),
-           "R4F": np.array([]),
-           "R4B": np.array([]),
-           "R5F1": np.array([]),
-           "R5F2": np.array([]),
-           "R5F3": np.array([]),
-           "R5B1": np.array([]),
-           "R5B2": np.array([]),
-           "R5B3": np.array([]),
-           "R6F": np.array([]),
-           "R6B": np.array([])}
-qz_urc =  {"R1F": np.array([]),
-           "R1B": np.array([]),
-           "R2F": np.array([]),
-           "R2B": np.array([]),
-           "R3F": np.array([]),
-           "R3B": np.array([]),
-           "R4F": np.array([]),
-           "R4B": np.array([]),
-           "R5F1": np.array([]),
-           "R5F2": np.array([]),
-           "R5F3": np.array([]),
-           "R5B1": np.array([]),
-           "R5B2": np.array([]),
-           "R5B3": np.array([]),
-           "R6F": np.array([]),
-           "R6B": np.array([])}
-qz_ulc =  {"R1F": np.array([]),
-           "R1B": np.array([]),
-           "R2F": np.array([]),
-           "R2B": np.array([]),
-           "R3F": np.array([]),
-           "R3B": np.array([]),
-           "R4F": np.array([]),
-           "R4B": np.array([]),
-           "R5F1": np.array([]),
-           "R5F2": np.array([]),
-           "R5F3": np.array([]),
-           "R5B1": np.array([]),
-           "R5B2": np.array([]),
-           "R5B3": np.array([]),
-           "R6F": np.array([]),
-           "R6B": np.array([])}
-qz_zcom =  {"R1F": np.array([]),
-           "R1B": np.array([]),
-           "R2F": np.array([]),
-           "R2B": np.array([]),
-           "R3F": np.array([]),
-           "R3B": np.array([]),
-           "R4F": np.array([]),
-           "R4B": np.array([]),
-           "R5F1": np.array([]),
-           "R5F2": np.array([]),
-           "R5F3": np.array([]),
-           "R5B1": np.array([]),
-           "R5B2": np.array([]),
-           "R5B3": np.array([]),
-           "R6F": np.array([]),
-           "R6B": np.array([])}
+front_quartz = front_quartz[front["Part"].str.contains("Quartz:1", na=False)]
+back_quartz  = back_quartz[back["Part"].str.contains("Quartz:1", na=False)]
+
+gdml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>             \n
+        <gdml xmlns:gdml=\"http://cern.ch/2001/Schemas/GDML\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" \
+         xsi:noNamespaceSchemaLocation=\"http://service-spi.web.cern.ch/service-spi/app/releases/GDML/schema/gdml.xsd\"> \n
+        <define>                                               \n
+        </define>                                              \n
+        <materials>                                            \n
+        </materials>                                           \
+       "
+
+f = open(args.out_file, "w")
+f.write(gdml)
+f.close()
+        
